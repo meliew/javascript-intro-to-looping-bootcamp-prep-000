383d7fece810cdda601536cb73361507b25b4d39
Melissa Williams <melissa@brandmeetscopy.com>
2019-03-07 21:57:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -28,12 +28,13 @@ function whileLoop(n) {
 
 function doWhileLoop(num) {
   function incrementVariable() {
-  num = num + 1;
-  return num;
+    num = num + 1;
+    return num;
 }
 
 do {
   console.log("I run once regardless.");
-} while (incrementVariable() < num);
+  }
+  while (incrementVariable() < num);
 
 }
